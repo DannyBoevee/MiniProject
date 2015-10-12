3158ac682ba7fd41ad8c7e8d9d10876c63e6b219
Danny Boevee <danny.boevee@student.hu.nl>
2015-10-12 15:28:41 +0200

    Huidige Datum Methode

--- Api.py
+++ Api.py
@@ -1,6 +1,17 @@
+import requests
+import time
+
 class Api:
     api_key = None
 
     def __init__(self):
         api_key = '9smtwozmiugid4m8m5f9n6g1oaiwt6r4'
-        api_web = ''
+        api_web = 'http://www.filmtotaal.nl/api/filmsoptv.xml?apikey='
+
+    def getCurrentTime(self):
+        """
+            Krijg huidige datum
+        :return:
+            Dag - Maand - Jaar
+        """
+        return time.strftime(time.strftime("%d-%m-%Y"))
