3be119ec4c52ef25da4b536603809a8e657f32de
Danny Boevee <danny.boevee@student.hu.nl>
2015-10-14 08:12:35 -0700

    Qr code iets groter

--- qrCode.py
+++ qrCode.py
@@ -7,8 +7,8 @@ class qRCode:
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
+            box_size=20,
+            border=2,
             )
         qr.add_data(data)
         qr.make(fit=True)
